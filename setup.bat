92f63a92e526987a0fe9fd41b2870cd616a43359
Diego2110w10 <diegoeren21@gmail.com>
2024-04-11 11:42:14 -0300

    setup feito e testado

---
+++ setup.bat
@@ -0,0 +1,9 @@
+ git config --global user.name "Diego2110w10"
+ git config --global user.email "diegoeren21@gmail.com"
+
+cd C:\Users\MAUA-MERCEDESVALENTI
+
+git clone https://github.com/Diego2110w10/Caesar.git
+cd Caesar
+
+code .
